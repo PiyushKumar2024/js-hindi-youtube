670f97c25af614f82f88a2dcb1c092e1b910b358
PiyushKumar2024 <piyushky2005@gmail.com>
2026-05-15 14:07:01 +0530

    Comment out comparison examples and add explanations
    Add explanations for null and undefined comparisons

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -5,12 +5,22 @@
 // console.log(2 != 1);
 
 
-// console.log("2" > 1);
+// console.log("2" > 1); convert them to the numbers
 // console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+//  1.console.log(null > 0); // false
+// Comparison (>) converts null to a number before comparing.
+// null is converted to 0.
+// 0 > 0 is false.
+
+// 2. console.log(null == 0); // false
+// The equality check (==) does not convert null to a number.
+// null is only equal to undefined in loose equality (==), not to numbers.
+// So, null == 0 is false.
+
+// 3. console.log(null >= 0); // true
+// The >= comparison also converts null to a number (0).
+// It becomes 0 >= 0, which is true.
 
 console.log(undefined == 0);
 console.log(undefined > 0);
