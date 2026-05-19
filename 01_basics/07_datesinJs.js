014579112f8286ce9e75aa39c1108de7fa57f035
PiyushKumar2024 <piyushky2005@gmail.com>
2026-05-19 15:36:02 +0530

    Update 07_datesinJs.js

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -4,9 +4,18 @@ let myDate = new Date()
 // console.log(myDate.toString());
 // console.log(myDate.toDateString());
 // console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
+// console.log(typeof myDate); //obj
 
-// let myCreatedDate = new Date(2023, 0, 23)
+console.log(myDate.toString()) //Output : Mon Oct 09 2023 07:39:18 GMT-0700 (Pacific Daylight Time)
+console.log(myDate.toLocaleString()) // Output : 10/9/2023, 7:44:20 AM
+console.log(myDate.toLocaleDateString()) //Output : 10/9/2023
+console.log(myDate.toLocaleTimeString()) // Output : 7:45:31 AM
+console.log(myDate.toDateString()) //Output : Mon Oct 09 2023
+console.log(myDate.toTimeString()) // Output : 07:46:40 GMT-0700 (Pacific Daylight Time)
+console.log(myDate.toISOString()) // Output : 2023-10-09T14:43:39.337Z
+console.log(myDate.toJSON()) //Output : 2023-10-09T14:40:58.495Z
+
+// let myCreatedDate = new Date(2023, 0, 23) //month 0 indexed
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 // let myCreatedDate = new Date("2023-01-14")
 let myCreatedDate = new Date("01-14-2023")
@@ -15,12 +24,12 @@ let myCreatedDate = new Date("01-14-2023")
 let myTimeStamp = Date.now()
 
 // console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+// console.log(myCreatedDate.getTime()); //convert date to milisec
+// console.log(Math.floor(Date.now()/1000)); //convert to seconds
 
 let newDate = new Date()
 console.log(newDate);
-console.log(newDate.getMonth() + 1);
+console.log(newDate.getMonth() + 1);//coz start from 0
 console.log(newDate.getDay());
 
 // `${newDate.getDay()} and the time `
