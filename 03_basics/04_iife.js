7157cec923a4d59f105c400b2ad2f4570b5c7ce7
PiyushKumar2024 <piyushky2005@gmail.com>
2026-05-22 09:24:35 +0530

    Update 04_iife.js

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,13 +1,15 @@
 // Immediately Invoked Function Expressions (IIFE)
 prevent from global pollution
 
-
+named iife
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
-})();
+})(); semicolon for multiple iife
 
+unamed iife
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
 } )('hitesh')
+parameter passing
 
